e29ec6b953115de32447def6099b4a48ca616f5a
C32-2 <vasilybichutsky@gmail.com>
2025-05-29 21:02:23 +0300

    Edited start activity

--- app/src/main/java/com/example/educationalchallenge/StartActivity.java
+++ app/src/main/java/com/example/educationalchallenge/StartActivity.java
@@ -2,10 +2,17 @@ package com.example.educationalchallenge;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -14,7 +21,9 @@ import androidx.core.view.WindowInsetsCompat;
 public class StartActivity extends AppCompatActivity {
 
     private Button loginButton;
-    private Button registerButton;
+    private TextView registerTextView;
+    private EditText emailEditText;
+    private EditText passwordEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,23 +32,40 @@ public class StartActivity extends AppCompatActivity {
         setContentView(R.layout.activity_start);
 
         loginButton = findViewById(R.id.login_button);
-        registerButton = findViewById(R.id.register_button);
-
-        registerButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(StartActivity.this, RegisterActivity.class);
-                startActivity(intent);
-            }
-        });
+        emailEditText = findViewById(R.id.email_edit_text);
+        passwordEditText = findViewById(R.id.password_edit_text);
+        registerTextView = findViewById(R.id.register_text);
 
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(StartActivity.this, LoginActivity.class);
-                startActivity(intent);
+
             }
         });
+
+        String text = registerTextView.getText().toString();
+        SpannableString spannableString = new SpannableString(text);
+        int startIndex = text.indexOf("Зарегистрироваться");
+        int endIndex = startIndex + "Зарегистрироваться".length();
+
+        ClickableSpan clickableSpan = new ClickableSpan() {
+            @Override
+            public void onClick(@NonNull View widget) {
+                Intent intent = new Intent(StartActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+
+            @Override
+            public void updateDrawState(@NonNull android.text.TextPaint ds) {
+                ds.setUnderlineText(false);
+                ds.setColor(ds.linkColor);
+            }
+        };
+
+        spannableString.setSpan(clickableSpan, startIndex, endIndex, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        registerTextView.setText(spannableString);
+        registerTextView.setMovementMethod(LinkMovementMethod.getInstance());
+        registerTextView.setHighlightColor(android.graphics.Color.TRANSPARENT);
     }
 
 }
