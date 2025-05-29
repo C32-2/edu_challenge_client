4090116ddd3091251d88a192377d90ffb6454bcd
C32-2 <vasilybichutsky@gmail.com>
2025-05-29 13:55:10 +0300

    Added custom colors. Added start activity

---
+++ app/src/main/java/com/example/educationalchallenge/StartActivity.java
@@ -0,0 +1,27 @@
+package com.example.educationalchallenge;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class StartActivity extends AppCompatActivity {
+
+    private Button loginButton;
+    private Button registerButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_start);
+    }
+
+    public void goToRegisterActivity() {
+
+    }
+}
