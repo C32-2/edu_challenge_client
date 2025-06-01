e534c2ffa9ee17421276c47f5be859eea32909f2
C32-2 <vasilybichutsky@gmail.com>
2025-06-01 12:52:48 +0300

    Added bottom navigation view

--- app/src/main/java/com/example/educationalchallenge/MainActivity.java
+++ app/src/main/java/com/example/educationalchallenge/MainActivity.java
@@ -1,24 +1,69 @@
 package com.example.educationalchallenge;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+
+import com.example.educationalchallenge.fragments.EditorFragment;
+import com.example.educationalchallenge.fragments.MainFragment;
+import com.example.educationalchallenge.fragments.ProfileFragment;
+import com.example.educationalchallenge.fragments.StatisticsFragment;
+import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation_view);
+
+        MaterialToolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportFragmentManager().
+                beginTransaction()
+                .replace(R.id.fragment_container, new MainFragment())
+                .commit();
+
+        bottomNavigationView.setOnItemSelectedListener(item -> handleNavigationItemSelected(item));
+    }
+
+    private boolean handleNavigationItemSelected(MenuItem item) {
+        Fragment currentFragment = null;
+        int id = item.getItemId();
+
+        if (id == R.id.nav_main) {
+            currentFragment = new MainFragment();
+        } else if (id == R.id.nav_statistics) {
+            currentFragment = new StatisticsFragment();
+        }
+        else if (id == R.id.nav_editors) {
+            currentFragment = new EditorFragment();
+        }
+        else if (id == R.id.nav_profile) {
+            currentFragment = new ProfileFragment();
+        }
+
+        if (currentFragment != null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, currentFragment)
+                    .commit();
+            getSupportActionBar().setTitle(item.getTitle());
+            return true;
+        }
+        return false;
     }
 }
---
+++ app/src/main/java/com/example/educationalchallenge/fragments/StatisticsFragment.java
@@ -0,0 +1,20 @@
+package com.example.educationalchallenge.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.educationalchallenge.R;
+
+public class StatisticsFragment extends Fragment {
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater,
+            ViewGroup container,
+            Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_statistics, container, false);
+    }
+}
